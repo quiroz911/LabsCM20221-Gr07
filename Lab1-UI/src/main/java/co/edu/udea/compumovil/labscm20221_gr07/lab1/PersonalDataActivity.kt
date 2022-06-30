6ddd549f30ce2547dcf2f2cdf670acba5ec415ff
quiroz911 <jpablo.quiroz@udea.edu.co>
2022-06-30 18:49:37 -0500

    creacion constraint layout

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labscm20221_gr07/lab1/PersonalDataActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.labscm20221_gr07.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonalDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_data)
+    }
+}
